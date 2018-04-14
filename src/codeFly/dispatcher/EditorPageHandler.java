a75f310a2cb595f058b87764cae2d14b4912e922
Shenyang Cai <sycai@outlook.com>
2018-04-14 19:22:54 -0400

    Testing ajax API

--- src/codeFly/dispatcher/EditorPageHandler.java
+++ src/codeFly/dispatcher/EditorPageHandler.java
@@ -4,22 +4,40 @@ import codeFly.CodeFly;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.file.Files;
 
 public class EditorPageHandler implements HttpHandler{
     @Override
     public void handle (HttpExchange exchange) throws IOException{
         // FIXME: request should provide the question number and server should return the proper question description
-        File editorPage = new File(CodeFly.ROOT_DIR + "frontEnd/editor.html");
-        // response with a success response
-        exchange.sendResponseHeaders(200, editorPage.length());
-        OutputStream os = exchange.getResponseBody();
-        Files.copy(editorPage.toPath(), os);
-        os.close();
-        CodeFly.logger.info(String.format("Sending %s to client: %S",
-                editorPage.getName(), exchange.getRemoteAddress()));
+        String method = exchange.getRequestMethod();
+        if (method.equalsIgnoreCase("GET")) {
+            File editorPage = new File(CodeFly.ROOT_DIR + "frontEnd/editor.html");
+            // response with a success response
+            exchange.sendResponseHeaders(200, editorPage.length());
+            OutputStream os = exchange.getResponseBody();
+            Files.copy(editorPage.toPath(), os);
+            os.close();
+            CodeFly.logger.info(String.format("Sending %s to client: %S",
+                    editorPage.getName(), exchange.getRemoteAddress()));
+        } else if (method.equalsIgnoreCase("POST")) {
+            String query = exchange.getRequestURI().getQuery();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+            String body = "";
+            String line;
+            while ((line = reader.readLine()) != null) {
+                body += line;
+            }
+            System.out.println(body);
+
+            String[] pairs = query.split("&");
+
+            exchange.sendResponseHeaders(200, query.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(query.getBytes());
+            os.close();
+        }
     }
 }
