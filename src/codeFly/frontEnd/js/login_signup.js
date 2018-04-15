baab6daf2f9d439c0da6bdbe73544e6354a8f02a
batistafei <dpf13992845139@gmail.com>
2018-04-15 14:44:15 -0400

    some changes in index.html, login.html and login_signup.js

--- src/codeFly/frontEnd/js/login_signup.js
+++ src/codeFly/frontEnd/js/login_signup.js
@@ -2,42 +2,33 @@ $(document).ready(function(){
 
 	$('#login').click(function(){
 
-		$.post("#url",       //$.post(URL,data,callback)
+		$.post("http://localhost:8080/login",       //$.post(URL,data,callback)
 			{
 				username: $("#exampleInputEmail1").val(),
 				password: $("#exampleInputPassword1").val()
 			},
-			function(data,status){
+			function(data){
 				//process the data
-				if(status ==="200"){
-					$("").innerHTML(data);        //not sure, after get the html from server, how to refresh the page, or use window.location.  method
-				}
-				else{
-					//handle ecxeptions
-				}
-			}
-		});
+				var newDoc = document.open("text/html", "replace");
+				newDoc.write(data);
+				newDoc.close();
+			});
 	});
 });
 
 $(document).ready(function(){
 
-	$('#signup').click(function(){
+	$('#register').click(function(){
 
-		$.post("#url",       //$.post(URL,data,callback)
+		$.post("http://localhost:8080/register",       //$.post(URL,data,callback)
 			{
 				username: $("#exampleInputEmail1").val(),
 				password: $("#exampleInputPassword1").val()
 			},
-			function(data,status){
-				//process the data
-				if(status ==="200"){
-					$("").innerHTML(data);        //not sure, after get the html from server, how to refresh the page, or use window.location.  method
-				}
-				else{
-					//handle ecxeptions
-				}
-			}
-		});
+			function(data){
+				var newDoc = document.open("text/html", "replace");
+				newDoc.write(data);
+				newDoc.close();
+			});
 	});
 });
