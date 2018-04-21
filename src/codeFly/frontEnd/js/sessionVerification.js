f02028b6f5c6eb101b333766bdbd13b01640e2e6
Shenyang Cai <sycai@outlook.com>
2018-04-21 11:43:53 -0400

    Polished verification process

--- src/codeFly/frontEnd/js/sessionVerification.js
+++ src/codeFly/frontEnd/js/sessionVerification.js
@@ -1,6 +1,17 @@
-function checkValidSession(obj) {
-    if (obj.userActive === false) {
-        window.location.href = "/login";
-    }
-    return obj.userActive;
+function checkValidSession(func) {
+    $.ajax({
+        type: 'GET',
+        url: '/verification',
+        success: function(data) {
+            var obj = JSON.parse(data);
+            if (obj.userActive === true) {
+                func();
+            } else {
+                window.location.href = '/login';
+            }
+        },
+        error: function() {
+           console.log('Error: ');
+        }
+    })
 }
